1eec162e5fe681134620f458d9e5e831bef5fc9e
Babita Bisht <84259086+Bbisht01@users.noreply.github.com>
2022-03-16 15:09:47 +0530

    BrouserRouter

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -3,6 +3,7 @@ import {Route,Routes} from "react-router"
 import About from "../components/About"
 import Home from "../components/Home"
 import { MoviesDashboard } from "../components/MoviesDashboard"
+import { SingleMovieDetails } from "../components/SingleMovieDetails"
 
 
 
@@ -12,6 +13,7 @@ export const MainRoutes = ()=>{
             <Route path="/" element={<Home></Home>} />
             <Route path="/about" element={<About></About>} />
             <Route path="/movies" element={<MoviesDashboard></MoviesDashboard>} />  
+            <Route path="/movies/:id" element={<SingleMovieDetails></SingleMovieDetails>}/>
         </Routes>      
     )
 }
