d5976033066793153caee3d69c25c77344d4bb7e
Babita Bisht <84259086+Bbisht01@users.noreply.github.com>
2022-03-16 15:03:09 +0530

    initial-Commit

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,6 +1,17 @@
+import React from "react"
+import {Route,Routes} from "react-router"
+import About from "../components/About"
+import Home from "../components/Home"
+import { MoviesDashboard } from "../components/MoviesDashboard"
+
+
 
 export const MainRoutes = ()=>{
-    return(
-        <></>
+    return(       
+        <Routes>
+            <Route path="/" element={<Home></Home>} />
+            <Route path="/about" element={<About></About>} />
+            <Route path="/movies" element={<MoviesDashboard></MoviesDashboard>} />  
+        </Routes>      
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,38 @@
+import { NavLink } from "react-router-dom";
+
 const links = [
   {
     title: "Home",
     link: "/",
     id: "header-link-home",
   },
+  {
+    title: "About",
+    link: "/about",
+    id: "header-link-about",
+  },
+  {
+    title: "movies",
+    link: "/movies",
+    id: "header-link-movies",
+  },
   //   add the other link as well
 ];
 export const Navbar = () => {
-  return {
-    //map through the link ad display it in header
-  };
+  return (
+    <div style={{backgroundColor:"grey",height:"40px"}}>
+{links.map(({title,link},index)=>{
+  return (    
+    <NavLink
+      key={index}
+      link={link}>      
+      {title}
+    </NavLink>   
+  );
+})}
+</div>
+
+
+  ); 
+  
 };
